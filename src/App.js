7bcdd9f7609feff93f574386b21c65e0daa86fc2
QuangNg14 <nnquang.1412@gmail.com>
2022-06-18 15:46:02 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { useEffect, useState } from "react";
 import Person from "./components/Person";
 
 const data = [
   {
+    index: 0,
     name: "Quang",
     age: 21,
     city: "NYC",
   },
   {
+    index: 1,
     name: "A",
     age: 20,
     city: "NYC",
   },
   {
+    index: 2,
     name: "B",
     age: 19,
     city: "NYC",
   },
   {
+    index: 3,
     name: "C",
     age: 18,
     city: "NYC",
@@ -24,15 +29,70 @@ const data = [
 ]; //array
 
 const App = () => {
-  const onClick = () => {
-    console.log("click in App component");
+  // const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [age, setAge] = useState(0);
+  const [personData, setPersonData] = useState(data);
+
+  const deleteThisItem = (index) => {
+    setPersonData(personData.filter((person) => person.index != index));
   };
+
   return (
     <div className="App">
-      {/* props */}
-      {data.map((person) => {
+      {/* <div>Count: {count}</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase Count
+      </button> */}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <input
+        type="number"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          // setPersonData(
+          //   personData.concat([
+          //     {
+          //       name,
+          //       city,
+          //       age,
+          //     },
+          //   ])
+          // );
+
+          setPersonData([
+            ...personData,
+            { name, age, city, index: personData.length },
+          ]);
+        }}
+      >
+        Add new person
+      </button>
+      {personData.map((person) => {
         return (
-          <Person name={person.name} age={person.age} city={person.city} />
+          <Person
+            index={person.index}
+            name={person.name}
+            age={person.age}
+            city={person.city}
+            deleteThisItem={deleteThisItem}
+          />
         );
       })}
     </div>
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import React from "react";
 
 const Person = (props) => {
+  const [age, setAge] = useState(props.age);
+  console.log(props.index);
   return (
-    <div onClick={props.onClick}>
-      My name is {props.name}. I am {props.age}. I am living in {props.city}
+    <div>
+      My name is {props.name}. I am {age}. I am living in {props.city}
+      <button onClick={(e) => console.log(e.target.id)}>Delete</button>
     </div>
   );
 };
